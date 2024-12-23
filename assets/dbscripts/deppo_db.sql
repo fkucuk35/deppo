97835d9c2fdd75e50d2f8eecf2c004dd98369e83
Fatih Küçük <fatihkucuk@live.com>
2024-12-23 03:30:20 +0300

    Veri Tabanı scripti düzenlendi

--- assets/dbscripts/deppo_db.sql
+++ assets/dbscripts/deppo_db.sql
@@ -1,22 +1,81 @@
+
 --
--- Veritabanı: `deppo_db`
+-- Veritabanı: `deppo_db` (23.12.2024)
 --
 
 -- --------------------------------------------------------
+--
+-- Tablo için tablo yapısı `deppo_order_detail_list`
+--
 
+CREATE TABLE `deppo_order_detail_list` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `stock_id` int(11) NOT NULL,
+  `order_quantity` smallint(5) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+-- --------------------------------------------------------
+--
+-- Tablo için tablo yapısı `deppo_order_list`
+--
+
+CREATE TABLE `deppo_order_list` (
+  `id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL,
+  `number` varchar(20) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+-- --------------------------------------------------------
+--
+-- Tablo döküm verisi `deppo_personel_list`
+--
+
+INSERT INTO `deppo_order_list` (`id`, `supplier_id`, `number`, `description`) VALUES
+(1, 1, 'SIP-2024-0001', 'Deneme Açıklama');
+
+-- --------------------------------------------------------
 --
 -- Tablo için tablo yapısı `deppo_personel_list`
 --
 
 CREATE TABLE `deppo_personel_list` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `surname` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `image_url` varchar(100) NOT NULL,
   `active` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+--
+-- Tablo döküm verisi `deppo_personel_list`
+--
 
+INSERT INTO `deppo_personel_list` (`id`, `name`, `image_url`, `active`) VALUES
+(1, 'Fatih KÜÇÜK', 'FATIH-KUCUK.jpg', 'ü');
+
+-- --------------------------------------------------------
+--
+-- Tablo için tablo yapısı `deppo_receipt_types`
+--
+
+CREATE TABLE `deppo_receipt_types` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+-- --------------------------------------------------------
+--
+-- Tablo döküm verisi `deppo_receipt_types`
+--
+
+INSERT INTO `deppo_receipt_types` (`id`, `name`) VALUES
+(1, 'Alım Fişi'),
+(2, 'Sarf Fişi');
+
+-- --------------------------------------------------------
 --
 -- Tablo için tablo yapısı `deppo_stock_card_list`
 --
@@ -30,6 +89,38 @@ CREATE TABLE `deppo_stock_card_list` (
   `active` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+--
+-- Tablo döküm verisi `deppo_stock_card_list`
+--
+
+INSERT INTO `deppo_stock_card_list` (`id`, `code`, `name`, `quantity`, `image_url`, `active`) VALUES
+(1, 'M8X15BB', 'Metrik 8x15 Bombebaş Civata', 0, 'M8X15BB.jpg', 'ü'),
+(2, 'M8X20BB', 'Metrik 8x20 Bombebaş Civata', 0, 'M8X20BB.jpg', 'ü');
+
+-- --------------------------------------------------------
+--
+-- Tablo için tablo yapısı `deppo_supplier_list`
+--
+
+CREATE TABLE `deppo_supplier_list` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `tax_office` varchar(100) NOT NULL,
+  `tax_number` varchar(20) NOT NULL,
+  `bill_address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
+
+-- --------------------------------------------------------
+--
+-- Tablo döküm verisi `deppo_supplier_list`
+--
+
+INSERT INTO `deppo_supplier_list` (`id`, `name`, `address`, `tax_office`, `tax_number`, `bill_address`) VALUES
+(1, 'MER TEKNİK', 'İZMİR', '', '', '');
+
+-- --------------------------------------------------------
 --
 -- Tablo için tablo yapısı `deppo_users`
 --
@@ -39,64 +130,152 @@ CREATE TABLE `deppo_users` (
   `username` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `surname` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `image_url` varchar(100) NOT NULL,
   `active` varchar(1) NOT NULL,
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `user_type` varchar(20) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_turkish_ci;
 
+-- --------------------------------------------------------
 --
 -- Tablo döküm verisi `deppo_users`
 --
 
-INSERT INTO `deppo_users` (`username`, `email`, `password`, `name`, `surname`, `active`, `user_type`) VALUES
-('fkucuk4235', 'fatihkucuk@live.com', '$2y$10$8TPm05Cpny6kMzBv5JImJuRV/1/3Gapg1UP5vzc/26EV7vdGDAGXe', 'Fatih', 'Küçük', 'ü', 'admin');
+INSERT INTO `deppo_users` (`id`, `username`, `email`, `password`, `name`, `image_url`, `active`, `user_type`) VALUES
+(1, 'fkucuk4235', 'fatihkucuk@live.com', '3c308e560d3fb3508b166bca45c3cb93', 'Fatih KÜÇÜK', 'FATIH-KUCUK.jpg', 'ü', 'admin');
 
+-- --------------------------------------------------------
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
 
+-- --------------------------------------------------------
+--
+-- Tablo için indeksler `deppo_order_detail_list`
+--
+
+ALTER TABLE `deppo_order_detail_list`
+  ADD PRIMARY KEY (`id`);
+
+-- --------------------------------------------------------
+--
+-- Tablo için indeksler `deppo_order_list`
+--
+
+ALTER TABLE `deppo_order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `number` (`number`);
+
+-- --------------------------------------------------------
 --
 -- Tablo için indeksler `deppo_personel_list`
 --
+
 ALTER TABLE `deppo_personel_list`
   ADD PRIMARY KEY (`id`);
 
+-- --------------------------------------------------------
+--
+-- Tablo için indeksler `deppo_receipt_types`
+--
+
+ALTER TABLE `deppo_receipt_types`
+  ADD PRIMARY KEY (`id`);
+
+-- --------------------------------------------------------
 --
 -- Tablo için indeksler `deppo_stock_card_list`
 --
+
 ALTER TABLE `deppo_stock_card_list`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `code` (`code`);
 
+-- --------------------------------------------------------
+--
+-- Tablo için indeksler `deppo_supplier_list`
+--
+
+ALTER TABLE `deppo_supplier_list`
+  ADD PRIMARY KEY (`id`);
+
+-- --------------------------------------------------------
 --
 -- Tablo için indeksler `deppo_users`
 --
+
 ALTER TABLE `deppo_users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`),
   ADD UNIQUE KEY `username` (`username`);
 
+-- --------------------------------------------------------
 --
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
 
+-- --------------------------------------------------------
+--
+-- Tablo için AUTO_INCREMENT değeri `deppo_order_detail_list`
+--
+
+ALTER TABLE `deppo_order_detail_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
+--
+-- Tablo için AUTO_INCREMENT değeri `deppo_order_list`
+--
+
+ALTER TABLE `deppo_order_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
 --
 -- Tablo için AUTO_INCREMENT değeri `deppo_personel_list`
 --
-ALTER TABLE `deppo_personel_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
+ALTER TABLE `deppo_personel_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
+--
+-- Tablo için AUTO_INCREMENT değeri `deppo_receipt_types`
+--
+
+ALTER TABLE `deppo_receipt_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
 --
 -- Tablo için AUTO_INCREMENT değeri `deppo_stock_card_list`
 --
 ALTER TABLE `deppo_stock_card_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+-- --------------------------------------------------------
+--
+-- Tablo için AUTO_INCREMENT değeri `deppo_supplier_list`
+--
+
+ALTER TABLE `deppo_supplier_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
 --
 -- Tablo için AUTO_INCREMENT değeri `deppo_users`
 --
+
 ALTER TABLE `deppo_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+-- --------------------------------------------------------
+--
+-- Görünüm için görünüm yapısı `deppo_order_list_view`
+--
+
+CREATE VIEW `deppo_order_list_view`
+AS SELECT `dol`.*, `dsl`.`name` `supplier_name`
+FROM `deppo_order_list` `dol`
+LEFT JOIN `deppo_supplier_list` `dsl` ON `dol`.`supplier_id`=`dsl`.`id`;
