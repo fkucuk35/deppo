026f27ae5c8012599bed5cef84c3c61918a266ca
Fatih Küçük <fatihkucuk@live.com>
2025-01-04 03:25:58 +0300

    inline cellediting öncesi son hata düzeltmeleri ve kontroller

--- partials/_header.php
+++ partials/_header.php
@@ -8,19 +8,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo $GLOBALS['ROOT']; ?>libs/font-awesome/css/all.min.css">
         <link rel="stylesheet" href="<?php echo $GLOBALS['ROOT']; ?>libs/bootstrap/css/bootstrap.min.css">
+            <script src="<?php echo $GLOBALS['ROOT']; ?>libs/bootstrap/js/bootstrap.bundle.min.js"></script>
             <title><?php echo $lang['app_title']; ?></title>
             <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/bootstrap/easyui.css">
-                <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/mobile.css"> 
-                    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/icon.css">
-                        <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/color.css"> 
-                            <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['ROOT']; ?>assets/css/style.css">
-                                <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['ROOT']; ?>assets/css/main.css">   
-                                    <script src="<?php echo $GLOBALS['ROOT']; ?>libs/bootstrap/js/bootstrap.bundle.min.js"></script>
-                                    <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>jquery.min.js"></script>
-                                    <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>jquery.easyui.min.js"></script>
-                                    <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>jquery.easyui.mobile.js"></script>
-                                    <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>locale/easyui-lang-tr.js"></script>
-                                    <script type="text/javascript" src="<?php echo $GLOBALS['ROOT']; ?>libs/notify.min.js"></script>
-                                    <script src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>extensions/datagrid-filter.js"></script>
-                                    </head>
-                                    <body>
+                <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/icon.css">
+                    <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>themes/color.css"> 
+                        <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['ROOT']; ?>assets/css/style.css">
+                            <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['ROOT']; ?>assets/css/main.css">   
+                                <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>jquery.min.js"></script>
+                                <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>jquery.easyui.min.js"></script>
+                                <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>locale/easyui-lang-tr.js"></script>
+                                <script type="text/javascript" src="<?php echo $GLOBALS['ROOT']; ?>libs/notify.min.js"></script>
+                                <script type="text/javascript" src="<?php echo $GLOBALS['LOCAL_EASYUI_ROOT']; ?>extensions/datagrid-filter.js"></script>
+                                </head>
+                                <body>
