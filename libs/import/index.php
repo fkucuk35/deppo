8603737559dc093f2a4047369345902b29b9f01d
Fatih Küçük <fatihkucuk@live.com>
2025-01-06 22:30:40 +0300

    Excelden toplu stok kartı sistemi kurulup Netsisten alınan stok kartları sisteme aktarıldı.

--- libs/import/index.php
+++ libs/import/index.php
@@ -1,13 +1,16 @@
 <?php
+date_default_timezone_set("Europe/Istanbul");
 include "vendor/autoload.php";
 include "cfg/dbconnect.php";
 $file_err = $err_msg = "";
 $result_import = "";
+$startTime = $finishTime = "";
 $valid_exts = array("xls", "xlsx");
 $upload_dir = "uploads/";
+$inProgress = false;
 if (isset($_POST['submit'])) {
     if ($_FILES['input_file']['name'] == "") {
-        $file_err = "Please select a file";
+        $file_err = "Lütfen bir excel dosyası seçiniz";
     } else {
         $file_name = $_FILES['input_file']['name'];
         $tmp_name = $_FILES['input_file']['tmp_name'];
@@ -18,6 +21,8 @@ if (isset($_POST['submit'])) {
             PATHINFO_EXTENSION
         ));
         if (in_array($ext, $valid_exts)) {
+            $inProgress = true;
+            $startTime = date("h:i:sa");
             $new_file = time() . "-" . basename($file_name);
             try {
                 move_uploaded_file($tmp_name, $upload_dir . $new_file);
@@ -33,19 +38,19 @@ if (isset($_POST['submit'])) {
                     $quantity = 0;
                     $image_url = "";
                     $active = "ü";
-                    $sql = "insert into excel (code, name, quantity, image_url, active) values(?,?,?,?,?)";
+                    $sql = "insert into deppo_stock_card_list (code, name, quantity, image_url, active) values(?,?,?,?,?)";
                     $stmt = $conn->prepare($sql);
                     $stmt->bind_param("ssiss", $code, $name, $quantity, $image_url, $active);
                     $stmt->execute();
-                    $result = $stmt->get_result();
-                    $result_count += $result->num_rows;
+                    $result_count += $stmt->affected_rows;
                 }
-                $result_import = $result->num_rows . " adet kayıt import edildi...";
+                $finishTime = date("h:i:sa");
+                $result_import = $result_count . " adet kayıt içeri aktarıldı...";
             } catch (Exception $e) {
                 $err_msg = $e->getMessage();
             }
         } else {
-            $err_msg = "Invalid File";
+            $err_msg = "Yanlış dosya formatı";
         }
     }
 } ?>
@@ -55,7 +60,7 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Import Excel data into MYSQL table</title>
+    <title>Excelden Stok Kartlarını İçeri Aktarma</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 </head>
@@ -63,7 +68,7 @@ if (isset($_POST['submit'])) {
 <body>
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>  -->
     <div class="container">
-        <h1>Import Excel data into MYSQL table</h1>
+        <h1>Excelden Stok Kartlarını İçeri Aktarma<?php if($inProgress) { echo "<br>Başlanma Zamanı: ".$startTime."<br>Bitiş Zamanı: ".$finishTime; } ?></h1>
         <?php
         if (!empty($err_msg)) { ?>
             <div class="alert alert-danger"><?php echo $err_msg; ?></div>
@@ -75,12 +80,12 @@ if (isset($_POST['submit'])) {
         ?>
         <form method="POST" enctype="multipart/form-data">
             <div class="mb-3">
-                <label for="input_file" class="form-label fw-bold">Upload file</label>
+                <label for="input_file" class="form-label fw-bold">Yüklenecek dosya</label>
                 <input type="file" name="input_file" id="input_file" class="form-control" aria-describedby="fileHelpId">
-                <div id="fileHelpId" class="form-text">Allowed File Type: xls, xlsx. Must have header line.</div>
+                <div id="fileHelpId" class="form-text">İzin verilen dosya formatı: xls, xlsx</div>
             </div>
             <div class="text-danger"><?php echo $file_err; ?></div>
-            <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+            <button type="submit" class="btn btn-primary" name="submit">İçeri Aktar</button>
         </form>
     </div>
 </body>
