065dee81d5f3ce1b08588139e2547ea3d938c70a
Fatih Küçük <fatihkucuk@live.com>
2025-01-16 19:45:25 +0300

    mobile app kaldırıldı

--- tools/mobile-app/ionic-barcode-scanner/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-barcode-scanner',
-  webDir: 'www'
-};
-
-export default config;
