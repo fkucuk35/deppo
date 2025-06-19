a88e4a18f78d490869fceba827186300eee09365
Fatih Küçük <fatihkucuk@live.com>
2025-06-19 17:34:00 +0300

    Veritabanı yedeği - 19.06.2025 17:34

--- assets/db_backup/deppo_db.sql
+++ assets/db_backup/deppo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 18 Haz 2025, 19:01:04
+-- Üretim Zamanı: 19 Haz 2025, 16:33:10
 -- Sunucu sürümü: 10.4.32-MariaDB
 -- PHP Sürümü: 8.2.12
 
